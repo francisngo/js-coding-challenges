f42613ed6815b931463052d428956cf103d90b9c
Francis Ngo <francis.t.ngo@gmail.com>
2023-02-22 09:22:23 -0700

    Solve productOfArray recursion problem

---
+++ recursion/productOfArray.ts
@@ -0,0 +1,14 @@
+/*
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+*/
+
+// solution
+const productOfArray = (nums: number[]): number => {
+    if (nums.length === 0) return 1;
+    return nums[0] * productOfArray(nums.slice(1));
+}
+
+
+// tests
+productOfArray([1,2,3]) // 6
+productOfArray([1,2,3,10]) // 60
