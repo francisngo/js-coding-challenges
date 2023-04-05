c007a4b603bc9fa0ff3b6e7393af1bb36f232220
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-05 10:29:37 -0700

    Update two sum

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -7,21 +7,21 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
+
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Example 2:
-
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 
 */
 
+// hash approach
 let twoSum = function(nums, target) {
     const map = {};
 
@@ -39,3 +39,20 @@ let twoSum = function(nums, target) {
         map[nums[i]] = i;
     }
 }
+
+// // two pointer approach
+// let twoSum = function(nums, target) {
+//     let left = 0;
+//     let right = 1;
+//     while (left < nums.length) {
+//         let sum = nums[left] + nums[right];
+//         if (sum === target) {
+//             return [left, right]
+//         } else if (right === nums.length - 1) {
+//             left++
+//             right = left + 1;
+//         } else {
+//             right++
+//         }
+//     }
+// }
