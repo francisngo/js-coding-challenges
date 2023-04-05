0e1eb6606ccdc1eed2d66e4fcab47557c74fce64
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-05 09:51:53 -0700

    Add container in water problem

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -13,8 +13,30 @@ https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
 Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+
 */
 
-let maxArea = function(height) {
-    
+let maxArea = function(heights) {
+    let maxArea = 0;
+    let left = 0
+    let right = heights.length - 1;
+    while (left < right) {
+        let height = Math.min(heights[left], heights[right])
+        let width = right - left;
+        let area = width * height;
+        maxArea = Math.max(maxArea, area)
+        if (heights[left] < heights[right]) {
+            left++;
+        } else if (heights[left] > heights[right]) {
+            right--;
+        } else {
+            left++;
+            right--;
+        }
+    }
+    return maxArea;
 }
