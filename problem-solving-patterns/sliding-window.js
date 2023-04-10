c2438dece1557e3b8bd9c2e071ff8b31ca0c0eee
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-10 16:07:48 -0700

    Add maximum average subarray

--- problem-solving-patterns/sliding-window.js
+++ problem-solving-patterns/sliding-window.js
@@ -151,9 +151,30 @@ function findBestSubarray(nums, k) {
 	// set currentSum to the output
 	let output = currentSum;
 	for (let j = k; j < nums.length; j++) {
+		// find sum by adding the next element and subtracting the previous element positioned at [i - k]
 		currentSum += nums[i] - nums[i - k];
 		output = Math.max(output, currentSum);
 	}
 
 	return output;
 }
+
+/* 
+Example 5: Maximum Average Subarray I
+
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+Example 1:
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Example 2:
+Input: nums = [5], k = 1
+Output: 5.00000
+
+*/
+
+function findMaxAverage(nums, k) {}
