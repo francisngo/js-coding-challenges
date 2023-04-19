cc4831bb57f661c19abb6f7bc78c4ee4fdb943aa
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-19 13:28:47 -0700

    Move 917 to two pointer folder

--- problem-solving-patterns/two-pointers/917-reverse-only-letters.js
+++ problem-solving-patterns/two-pointers/917-reverse-only-letters.js
@@ -18,16 +18,40 @@ Input: s = "Test1ng-Leet=code-Q!"
 Output: "Qedo1ct-eeLg=ntse-T!"
 */
 
+// regex approach
 var reverseOnlyLetters = function (s) {
 	let regex = new RegExp("[a-zA-Z]");
-	let characters = s.split("").filter((char) => regex.test(char));
-	let output = [];
+	let characters = s.split("").filter((char) => regex.test(char)); // O(n)
+	let output = []; // SC: O(n)
 	for (let i = 0; i < s.length; i++) {
+		// O(n)
 		if (regex.test(s[i])) {
 			output.push(characters.pop()); // O(1)
 		} else {
 			output.push(s[i]); // O(1)
 		}
 	}
-	return output.join("");
+	return output.join(""); // O(n)
 };
+
+// two pointer approach
+var reverseOnlyLetters = function (s) {
+	let regex = new RegExp("[a-zA-Z]");
+	let left = 0;
+	let right = s.length - 1;
+	while (left < right) {
+		if (regex.test(s[left]) && s[right]) {
+			let temp = s[left];
+			s[left] = s[right];
+			s[righ] = temp;
+			left++;
+			right--;
+		} else if (regex.test(s[left] && !regex.test(s[right]))) {
+			right--;
+		} else if (!regex.test(s[left] && regex.test(s[right]))) {
+			left++;
+		}
+	}
+};
+
+console.log(reverseOnlyLetters("a-bC-dEf-ghIj"));
