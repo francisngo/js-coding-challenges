6226f6a6725584a57a0803d809cd54b069d2e3e3
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-20 09:08:41 -0700

    Add notes to problem #643

--- problem-solving-patterns/sliding-windows/643-maximum-average-subarray-i.js
+++ problem-solving-patterns/sliding-windows/643-maximum-average-subarray-i.js
@@ -19,9 +19,12 @@ let findMaxAverage = function (nums, k) {
 	for (let i = 0; i < k; i++) {
 		currentSum += nums[i];
 	}
-
 	let maxSum = currentSum;
 	for (let i = k; i < nums.length; i++) {
+		// take the current sum, add the new current value and subtract the first value from the previous window
+		// [1, 12, -5, -6] has a sum of 2
+		// [12, -5, -6, 50] in this window, 1 is removed and 50 is added
+		// hence why currentSum (which is 2) += 50 - 1 = 51
 		currentSum += nums[i] - nums[i - k];
 		maxSum = Math.max(maxSum, currentSum);
 	}
