a79b86e93e83d8e7c053e9dd36d85d848fe5498c
Francis Ngo <francis.t.ngo@gmail.com>
2023-08-15 14:25:39 -0700

    Add sliding window code template

---
+++ problem-solving-patterns/sliding-windows/code-template.js
@@ -0,0 +1,20 @@
+const CONDITION = "CONDITION";
+
+var fn = (arr) => {
+	let left = 0;
+	let current = 0;
+	let answer = 0;
+
+	for (let right = 0; right < arr.length; right++) {
+		// do logic here to add arr[right] to current
+
+		while (CONDITION) {
+			// remove arr[left] from current
+			left++;
+		}
+
+		// update answer
+	}
+
+	return answer;
+};
