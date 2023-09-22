7964501527745dac0e7f84d5ce643cc3e6101341
Francis Ngo <francis.t.ngo@gmail.com>
2023-09-22 10:02:27 -0700

    Add max number of balloons

---
+++ 1189-maximum-number-of-balloons.js
@@ -0,0 +1,24 @@
+/*
+Given a string text, you want to use the characters of text to form as many instances of the word 'balloon' as possible. 
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+
+Example 3:
+Input: text = "leetcode"
+Output: 0
+*/
+var maxNumberOfBalloons = function (text) {
+	const map = { b: 0, a: 0, l: 0, o: 0, n: 0 };
+	for (let char of text) {
+		map[char]++;
+	}
+	return Math.floor(Math.min(map.b, map.a, map.l / 2, map.o / 2, map.n));
+};
